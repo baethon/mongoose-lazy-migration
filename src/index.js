329582f3ea15ed6c497f867b6fa2f7f3b7b64d0b
Radoslaw Mejer <radmen@baethon.com>
2019-12-30 21:21:11 +0100

    Add hasMigrations()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const {mutate} = require('@baethon/pure-mutation');
 
 const migration = (number, migrationFn) => {
 	assert.ok(typeof number === 'number');
@@ -7,4 +8,40 @@ const migration = (number, migrationFn) => {
 	return {version: number, migration: migrationFn};
 };
 
-module.exports = {migration};
+const applyPendingMigrations = migrations => document => {
+	const {schemaVersion} = document;
+
+	return migrations.reduce(
+		(newDoc, {version, migration}) => {
+			return (!schemaVersion || schemaVersion < version) ?
+				migration(newDoc) :
+				newDoc;
+		},
+		document
+	);
+};
+
+const hasMigrations = (schema, options) => {
+	const {
+		migrations = [],
+		index = true
+	} = options;
+
+	const [{version: latestVersion}] = migrations.slice(-1);
+
+	schema.add({
+		schemaVersion: {
+			type: Number,
+			default: latestVersion,
+			index
+		}
+	});
+
+	schema.pre('init', document => {
+		mutate(document, applyPendingMigrations(migrations));
+	});
+
+	return schema;
+};
+
+module.exports = {migration, hasMigrations};
---
+++ test/has-migrations.test.js
@@ -0,0 +1,100 @@
+import test from 'ava';
+import mongoose from 'mongoose';
+import SchemaNumber from 'mongoose/lib/schema/number';
+import {hasMigrations, migration as m} from '../src';
+
+const {Schema} = mongoose;
+let User;
+
+test.before(async () => {
+	await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	});
+
+	const userSchema = new Schema({
+		fullname: String,
+		updates: {
+			type: Number,
+			default: 0
+		}
+	});
+
+	User = mongoose.model('User', hasMigrations(userSchema, {
+		migrations: [
+			m(1, user => ({
+				...user,
+				fullname: `${user.firstname} ${user.lastname}`,
+				updates: user.updates + 1
+			})),
+			m(2, user => ({
+				fullname: user.fullname,
+				updates: user.updates + 1
+			}))
+		]
+	}));
+});
+
+test.after.always(() => {
+	User.deleteMany({});
+});
+
+test('it extends schema', t => {
+	const schemaVersion = User.schema.path('schemaVersion');
+
+	t.true(schemaVersion instanceof SchemaNumber);
+	t.is(2, schemaVersion.defaultValue);
+	t.true(schemaVersion._index);
+});
+
+test('it extends schema | disabled index', t => {
+	const schema = hasMigrations(new Schema({}), {
+		migrations: [
+			m(1, data => data)
+		],
+		index: false
+	});
+
+	t.false(schema.path('schemaVersion')._index);
+});
+
+test('it migrates document without any version', async t => {
+	const {insertedId} = await User.collection.insertOne({
+		firstname: 'Jon',
+		lastname: 'Snow',
+		updates: 0
+	});
+
+	const document = await User.findOne({_id: insertedId});
+
+	t.is('Jon Snow', document.fullname);
+	t.is(2, document.schemaVersion);
+	t.is(2, document.updates);
+});
+
+test('it applies only missing migration', async t => {
+	const {insertedId} = await User.collection.insertOne({
+		firstname: 'Jon',
+		lastname: 'Snow',
+		fullname: 'Jon Snow',
+		schemaVersion: 1,
+		updates: 0
+	});
+
+	const document = await User.findOne({_id: insertedId});
+
+	t.is(2, document.schemaVersion);
+	t.is(1, document.updates);
+});
+
+test('it makes no update to most recent version', async t => {
+	const {_id: insertedId} = await User.create({
+		fullname: 'Jon Snow'
+	});
+
+	const document = await User.findOne({_id: insertedId});
+
+	t.is('Jon Snow', document.fullname);
+	t.is(2, document.schemaVersion);
+	t.is(0, document.updates);
+});
